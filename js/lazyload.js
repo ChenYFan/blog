3a57b6ae7bc989c1c67d5b27d9a1ab664acb89b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 22:49:18 +0000

    Deploy ChenYFan/blog to github.com/ChenYFan/blog.git:gh-pages

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,53 +1,70 @@
-(function (window) {
-	var images = Array.prototype.slice.call(document.querySelectorAll('img[srcset]'));
+// eslint-disable-next-line no-unused-expressions
+!(function(window, document) {
+  var runningOnBrowser = typeof window !== 'undefined';
+  var supportsIntersectionObserver = runningOnBrowser && 'IntersectionObserver' in window;
 
-	function elementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		var height = window.innerHeight || document.documentElement.clientHeight;
-		return (
-			rect.top >= 0
-			&& rect.left >= 0
-			&& rect.top <= height * 3
-		);
-	}
-	function loadImage(el, fn) {
-		var img = new Image(), src = el.getAttribute('src');
-		img.onload = function () {
-			el.srcset = src;
-			fn ? fn() : null;
-		};
-		img.srcset = src;
-	}
+  var images = Array.prototype.slice.call(document.querySelectorAll('img[srcset]'));
+  if (!images || images.length === 0) {
+    return;
+  }
 
-	function processImages() {
-		for (var i = 0; i < images.length; i++) {
-			if (elementInViewport(images[i])) {
-				(function(index){
-					var loadingImage = images[index];
-					loadImage(loadingImage, function () {
-						images = images.filter(function(t) {
-							return loadingImage !== t;
-						});
-					});
-				})(i);
-			}
-		}
-		if (images.length === 0) {
-			window.removeEventListener('scroll', imageLazyLoader)
-		}
-	}
+  if (supportsIntersectionObserver) {
+    var io = new IntersectionObserver(function(changes) {
+      changes.forEach(({ target, isIntersecting }) => {
+        if (!isIntersecting) return;
+        target.setAttribute('srcset', target.src);
+        target.onload = target.onerror = () => io.unobserve(target);
+      });
+    }, {
+      threshold : [0],
+      rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
+    });
+    images.map((item) => io.observe(item));
+  } else {
+    // eslint-disable-next-line no-inner-declarations
+    function elementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      var height = window.innerHeight || document.documentElement.clientHeight;
+      var top = rect.top;
+      return (top >= 0 && top <= height * 3) || (top <= 0 && top <= -(height * 2) - rect.height);
+    }
 
-	function throttle(method, context) {
-		clearTimeout(method.tId);
-		method.tId = setTimeout(function () {
-			method.call(context);
-		}, 100);
-	}
+    // eslint-disable-next-line no-inner-declarations
+    function loadImage(el, fn) {
+      var img = new Image();
+      var src = el.getAttribute('src');
+      img.onload = function() {
+        el.srcset = src;
+        fn && fn();
+      };
+      img.srcset = src;
+    }
 
-	processImages();
-	let imageLazyLoader = function () {
-		throttle(processImages, window);
-	};
+    // eslint-disable-next-line no-undef
+    var lazyLoader = new Debouncer(processImages);
 
-	window.addEventListener('scroll', imageLazyLoader);
-})(this);
+    // eslint-disable-next-line no-inner-declarations
+    function processImages() {
+      for (var i = 0; i < images.length; i++) {
+        if (elementInViewport(images[i])) {
+          // eslint-disable-next-line no-loop-func
+          (function(index) {
+            var loadingImage = images[index];
+            loadImage(loadingImage, function() {
+              images = images.filter(function(t) {
+                return loadingImage !== t;
+              });
+            });
+          })(i);
+        }
+      }
+      if (images.length === 0) {
+        window.removeEventListener('scroll', lazyLoader, false);
+      }
+    }
+
+    window.addEventListener('scroll', lazyLoader, false);
+    lazyLoader.handleEvent();
+  }
+
+})(window, document);
--- js/main.js
+++ js/main.js
@@ -1,113 +1,123 @@
+// 监听滚动事件
+function listenScroll(callback) {
+  // eslint-disable-next-line no-undef
+  const dbc = new Debouncer(callback);
+  window.addEventListener('scroll', dbc, false);
+  dbc.handleEvent();
+}
+
+// 滚动到指定元素
 function scrollToElement(target, offset) {
   var scroll_offset = $(target).offset();
   $('body,html').animate({
     scrollTop: scroll_offset.top + (offset || 0),
-    easing: 'swing',
+    easing   : 'swing'
   });
 }
 
-function scrollToBoard() {
-  scrollToElement('#board', -$('#navbar').height());
-}
-
-// 防抖动函数
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this, args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
-
-$(document).ready(function () {
-  // 顶部菜单的动效
+// 顶部菜单的监听事件
+function navbarScrollEvent() {
   var navbar = $('#navbar');
+  var submenu = $('#navbar .dropdown-menu');
   if (navbar.offset().top > 0) {
-    navbar.addClass('navbar-custom');
     navbar.removeClass('navbar-dark');
+    submenu.removeClass('navbar-dark');
   }
-  $(window).scroll(function () {
+  listenScroll(function() {
+    navbar[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('top-nav-collapse');
+    submenu[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('dropdown-collapse');
     if (navbar.offset().top > 0) {
-      navbar.addClass('navbar-custom');
       navbar.removeClass('navbar-dark');
+      submenu.removeClass('navbar-dark');
     } else {
       navbar.addClass('navbar-dark');
+      submenu.removeClass('navbar-dark');
     }
   });
-  $('#navbar-toggler-btn').on('click', function () {
+  $('#navbar-toggler-btn').on('click', function() {
     $('.animated-icon').toggleClass('open');
     $('#navbar').toggleClass('navbar-col-show');
   });
+}
 
-  // 头图视差动画
+// 头图视差的监听事件
+function parallaxEvent() {
   var target = $('#background[parallax="true"]');
-  var parallax = function () {
+  var parallax = function() {
     var oVal = $(window).scrollTop() / 5;
-    var offset = parseInt($('#board').css('margin-top'));
+    var offset = parseInt($('#board').css('margin-top'), 0);
     var max = 96 + offset;
     if (oVal > max) {
       oVal = max;
     }
     target.css({
-      transform: 'translate3d(0,' + oVal + 'px,0)',
+      transform          : 'translate3d(0,' + oVal + 'px,0)',
       '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-o-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform'    : 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform'     : 'translate3d(0,' + oVal + 'px,0)'
     });
 
     var toc = $('#toc');
     if (toc) {
       $('#toc-ctn').css({
-        'padding-top': `${ oVal }px`,
+        'padding-top': oVal + 'px'
       });
     }
   };
   if (target.length > 0) {
-    parallax();
-    $(window).scroll(parallax);
+    listenScroll(parallax);
   }
+}
 
-  // 向下滚动箭头的点击
-  $('.scroll-down-bar').on('click', scrollToBoard);
+// 向下滚动箭头的监听事件
+function scrollDownArrowEvent() {
+  $('.scroll-down-bar').on('click', function() {
+    scrollToElement('#board', -$('#navbar').height());
+  });
+}
 
-  // 向顶部滚动箭头
+// 向顶部滚动箭头的监听事件
+function scrollTopArrowEvent() {
   var topArrow = $('#scroll-top-button');
+  if (!topArrow) {
+    return;
+  }
   var posDisplay = false;
   var scrollDisplay = false;
   // 位置
-  var setTopArrowPos = function () {
+  var setTopArrowPos = function() {
     var boardRight = document.getElementById('board').getClientRects()[0].right;
     var bodyWidth = document.body.offsetWidth;
     var right = bodyWidth - boardRight;
     posDisplay = right >= 50;
     topArrow.css({
       'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-      'right': right - 64 + 'px',
+      'right' : right - 64 + 'px'
     });
   };
   setTopArrowPos();
   $(window).resize(setTopArrowPos);
   // 显示
   var headerHeight = $('#board').offset().top;
-  $(window).scroll(debounce(function () {
+  listenScroll(function() {
     var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
     scrollDisplay = scrollHeight >= headerHeight;
     topArrow.css({
-      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-    });
-  }, 20));
-  // 点击
-  topArrow.on('click', function () {
-    $('body,html').animate({
-      scrollTop: 0,
-      easing: 'swing',
+      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
     });
   });
+  // 点击
+  topArrow.on('click', function() {
+    $('body,html').animate({
+      scrollTop: 0,
+      easing   : 'swing'
+    });
+  });
+}
+
+$(document).ready(function() {
+  navbarScrollEvent();
+  parallaxEvent();
+  scrollDownArrowEvent();
+  scrollTopArrowEvent();
 });
