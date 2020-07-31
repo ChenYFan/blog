325b2adddff35201a344f4c6bfa5778c671fc826
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-07-31 11:33:29 +0800

    Update sw.js

--- source/sw.js
+++ source/sw.js
@@ -58,3 +58,78 @@ workbox.routing.registerRoute(
       ],
     })
 );
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://img\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'img-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://assets\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'assets-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://blog\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'blog-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://cdn\.staticfile\.org/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'staticfile-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://cdn\.jsdelivr\.net/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'jsdelivr-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
