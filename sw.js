e8cd60993279f067c56570918c27c3b1b569d3a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-31 07:35:34 +0000

    Deploy ChenYFan/blog to github.com/ChenYFan/blog.git:gh-pages

--- sw.js
+++ sw.js
@@ -1,1 +1,135 @@
-"use strict";importScripts("https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js"),workbox.setConfig({modulePathPrefix:"https://g.alicdn.com/kg/workbox/3.3.0/"}),workbox?console.log("Yay! Workbox is loaded 🎉"):console.log("Boo! Workbox didn't load 😬"),workbox.routing.registerRoute(/.*\.css/,workbox.strategies.staleWhileRevalidate({cacheName:"css-cache"})),workbox.routing.registerRoute(/.*\.js/,workbox.strategies.staleWhileRevalidate({cacheName:"js-cache"})),workbox.routing.registerRoute(new RegExp("https://img-pixiv.cyfan.top/"),workbox.strategies.cacheFirst({cacheName:"gravatar-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://pixiv.cyfan.top/"),workbox.strategies.cacheFirst({cacheName:"gravatar-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://img.cyfan.top/"),workbox.strategies.cacheFirst({cacheName:"img-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://assets.cyfan.top/"),workbox.strategies.cacheFirst({cacheName:"assets-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://blog.cyfan.top/"),workbox.strategies.cacheFirst({cacheName:"blog-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://cdn.staticfile.org/"),workbox.strategies.cacheFirst({cacheName:"staticfile-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]})),workbox.routing.registerRoute(new RegExp("https://cdn.jsdelivr.net/"),workbox.strategies.cacheFirst({cacheName:"jsdelivr-cache",plugins:[new workbox.expiration.Plugin({maxAgeSeconds:2592e3})]}));
+'use strict';
+
+importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
+workbox.setConfig({
+    modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/'
+});
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+workbox.routing.registerRoute(
+    // Cache CSS files
+    /.*\.css/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'css-cache',
+    })
+);
+workbox.routing.registerRoute(
+    // Cache JS files
+    /.*\.js/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'js-cache',
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://img\-pixiv\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'gravatar-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://pixiv\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'gravatar-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://img\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'img-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://assets\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'assets-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://blog\.cyfan\.top/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'blog-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://cdn\.staticfile\.org/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'staticfile-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+workbox.routing.registerRoute(
+    // Cache gravatar files
+    new RegExp('https://cdn\.jsdelivr\.net/'),
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'jsdelivr-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache for a maximum of 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
