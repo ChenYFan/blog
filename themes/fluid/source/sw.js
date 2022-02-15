9f9d2d8795ff65b728ee15f277565a338857e5f1
Chen <chen2778754364@foxmail.com>
2022-02-15 17:20:43 +0800

    add

--- themes/fluid/source/sw.js
+++ themes/fluid/source/sw.js
@@ -5,6 +5,7 @@ self.db = {
         if (!config) { config = { type: "text" } }
         return new Promise((resolve, reject) => {
             caches.match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`)).then(function (res) {
+                if (!res) resolve(null)
                 res.text().then(text => resolve(text))
             }).catch(() => {
                 resolve(null)
@@ -214,6 +215,50 @@ const handle = async function (req) {
     const path = pathname.split('?')[0]
     const query = q => urlObj.searchParams.get(q)
     let urls = []
+    let msg = JSON.parse(await db.read('msg')) || (async () => { await db.write('msg', '[]'); return '[]' })()
+    const nqurl = urlStr.split('?')[0]
+    const nqreq = new Request(nqurl)
+    const cache_delete = async (url) => {
+        const cache = await caches.open(CACHE_NAME)
+        await cache.delete(url)
+    }
+
+    if (query('nosw') == 'true') {
+        return fetch(req)
+    }
+    if (query('delete') == 'true') {
+
+        cache_delete(nqreq);
+        msg.push(
+            {
+                "name": "文件已删除",
+                "time": new Date(),
+                "info": `已删除${nqurl}`
+            }
+        )
+        await db.write('msg', JSON.stringify(msg))
+        return new Response(JSON.stringify({ ok: 1 }))
+    }
+    if (query('forceupdate') == 'true') {
+        //update cache
+
+        msg.push(
+            {
+                "name": "文件已强制更新",
+                "time": new Date(),
+                "info": `已更新${nqurl}`
+            }
+        )
+        await db.write('msg', JSON.stringify(msg))
+        await fetch(req).then(function (res) {
+            return caches.open(CACHE_NAME).then(function (cache) {
+                cache_delete(nqreq);
+                cache.put(req, res.clone());
+                return res;
+            });
+        });
+        return new Response(JSON.stringify({ ok: 1 }))
+    }
     for (let i in cdn) {
         for (let j in cdn[i]) {
             //console.log(domain, cdn[i][j].url.split('https://')[1].split('/')[0])
@@ -239,6 +284,10 @@ const handle = async function (req) {
     }
     for (var i in blog.origin) {
         if (domain.split(":")[0] == blog.origin[i].split(":")[0]) {
+
+            if (urlStr.match(/\/blog\-cgi/g)) {
+                return handlecgi(req)
+            }
             if (blog.local) { return fetch(req) }
 
             urls = []
@@ -249,7 +298,7 @@ const handle = async function (req) {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
                     caches.match(req).then(function (resp) {
-                        if (!!resp && query('forceupdate') != 'true') {
+                        if (!!resp) {
                             console.log('Had Cache:' + urlStr)
                             setTimeout(() => {
                                 resolve(resp)
@@ -286,9 +335,9 @@ const handle = async function (req) {
                 });
             })*//*
             if (!n) {
-
+    
                 return new Response('<h1>ChenBlogHelper Error</h1>', { headers: { "content-type": "text/html; charset=utf-8" } })
-
+    
                 return caches.match(req)
             } else {
                 caches.open(CACHE_NAME).then(function (cache) {
@@ -367,16 +416,38 @@ const lfetch = async (urls, url) => {
     const PauseProgress = async (res) => {
         return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
     };
-    let results = Promise.any(urls.map(urls => {
-        return new Promise((resolve, reject) => {
+    let results = Promise.any(urls.map(async urls => {
+        return new Promise(async (resolve, reject) => {
             fetch(urls, {
                 signal: controller.signal
             })
                 .then(PauseProgress)
-                .then(res => {
+                .then(async res => {
                     const resn = res.clone()
                     if (resn.status == 200) {
-                        setTimeout(() => {
+                        setTimeout(async () => {
+                            db.write('HIT_HOT', await (async () => {
+                                const hit = await (async () => { try { return JSON.parse(await db.read('HIT_HOT')) || { site: {}, static: {} } } catch (e) { return { site: {}, static: {} } } })()
+                                const domain = urls.split('/')[2]
+                                //hit[domain] = hit[domain] ? hit[domain] + 1 : 1
+                                if (blog.plus.indexOf(domain) > -1) {
+                                    hit.site[domain] = hit.site[domain] ? hit.site[domain] + 1 : 1
+                                } else {
+                                    hit.static[domain] = hit.static[domain] ? hit.static[domain] + 1 : 1
+                                }
+                                return JSON.stringify(hit)
+                            })());
+                            db.write('HIT_HOT_SIZE', await (async () => {
+                                const hit = await (async () => { try { return JSON.parse(await db.read('HIT_HOT_SIZE')) || { site: {}, static: {} } } catch (e) { return { site: {}, static: {} } } })()
+                                const domain = urls.split('/')[2]
+                                //hit[domain] = hit[domain] ? hit[domain] + Number(res.headers.get('Content-Length')) : Number(res.headers.get('Content-Length'))
+                                if (blog.plus.indexOf(domain) > -1) {
+                                    hit.site[domain] = hit.site[domain] ? hit.site[domain] + Number(res.headers.get('Content-Length')) : Number(res.headers.get('Content-Length'))
+                                } else {
+                                    hit.static[domain] = hit.static[domain] ? hit.static[domain] + Number(res.headers.get('Content-Length')) : Number(res.headers.get('Content-Length'))
+                                }
+                                return JSON.stringify(hit)
+                            })())
                             ws_sw({
                                 type: "send",
                                 data: JSON.stringify({
@@ -407,23 +478,172 @@ const lfetch = async (urls, url) => {
 }
 
 
-/*
-const test_func = async () => {
-    for (let i in cdn) {
-        for (let j in cdn[i]) {
-            let t1 = new Date().getTime()
-            const n = await fetch(cdn[i][j].url + testurl[i] + '?' + Math.random())
-            let t2 = new Date().getTime()
-            if (n.status === 200) {
-                cdn[i][j].time = t2 - t1
-                console.log(`TEST:${cdn[i][j].url + testurl[i]} WITH ${t2 - t1}ms`)
-            } else {
-                cdn[i][j].time = 20000
-                console.log(`TEST:${cdn[i][j].url + testurl[i]} WITH Erorr ${t2 - t1}ms`)
-            }
+const handlecgi = async (req) => {
+    const intelligent_size = (byte) => {
+        if (byte < 1024) {
+            return `${byte}B`
+        } else if (byte < 1024 * 1024) {
+            return `${(byte / 1024).toFixed(2)}KB`
+        } else if (byte < 1024 * 1024 * 1024) {
+            return `${(byte / 1024 / 1024).toFixed(2)}MB`
+        } else {
+            return `${(byte / 1024 / 1024 / 1024).toFixed(2)}GB`
         }
     }
-    console.log(cdn)
+    const urlStr = req.url
+    let urlObj = new URL(urlStr)
+    const uuid = await db.read('uuid')
+    //console.log(uuid)
+    const pathname = urlObj.href.substr(urlObj.origin.length)
+    const query = q => urlObj.searchParams.get(q)
+    const endpoint = "https://npm.elemecdn.com/chenyfan-blog-helper-dash@0.0.1/"
+    let dash_main = await (await fetch(endpoint + 'index.html')).text()
+
+
+    const HIT_HOT = await (async () => { try { return JSON.parse(await db.read('HIT_HOT')) || {} } catch (e) { return {} } })()
+    const HIT_HOT_SIZE = await (async () => { try { return JSON.parse(await db.read('HIT_HOT_SIZE')) || {} } catch (e) { return {} } })()
+    const HIT_ALL = (() => {
+        p = {}
+        for (let i in HIT_HOT.site) {
+            p[i] = HIT_HOT.site[i]
+        }
+        for (let i in HIT_HOT.static) {
+            p[i] = HIT_HOT.static[i]
+        }
+        return p
+    })()
+    const HIT_ALL_SIZE = (() => {
+        p = {}
+        for (let i in HIT_HOT_SIZE.site) {
+            p[i] = HIT_HOT_SIZE.site[i]
+        }
+        for (let i in HIT_HOT_SIZE.static) {
+
+            p[i] = HIT_HOT_SIZE.static[i]
+        }
+        return p
+    })()
+    let msg = await (await fetch(endpoint + 'part/message.html')).text()
+    let msg_init = JSON.parse(await db.read('msg')) || []
+    console.log(msg_init)
+    //await fetch('https://test/'+JSON.stringify(msg_init))
+
+    const MSG_HTML = (() => {
+        let u = ""
+        for (let i in msg_init.reverse()) {
+            u += msg.replace(/<!--NAME-->/g, msg_init[i].name)
+                .replace(/<!--TIME-->/g, msg_init[i].time)
+                .replace(/<!--INFO-->/g, msg_init[i].info)
+
+        }
+        console.log(u)
+        return u
+    })()
+    dash_main = dash_main.replace(/<!--MESSAGE-->/g, MSG_HTML)
+    switch (query('dash')) {
+        case 'test':
+            let test_html = await (await fetch(endpoint + 'content/test.html')).text()
+            return new Response(dash_main
+                .replace(/<!--TEST_ACTIVE-->/g, ' active')
+                .replace(/<!--SECTION-->/g, test_html)
+                , {
+                    headers: {
+                        'Content-Type': 'text/html'
+                    }
+                })
+
+        case 'cache':
+            let cache_section_html = await (await fetch(endpoint + 'part/cache.html')).text()
+            let cache_content_html = await (await fetch(endpoint + 'content/cache.html')).text()
+            const cache_list = await new Promise((resolve, reject) => {
+                let p = []
+                caches.open(CACHE_NAME).then(async cache => {
+                    cache.keys().then(async res => {
+                        for (var i in res) {
+                            t = {}
+
+                            t.url = res[i].url
+                            if (t.url.match('https://localcache/')) {
+                                continue
+                            }
+                            const ress = await cache.match(res[i])
+                            t.size = Number(ress.headers.get('content-length'))
+                            t.time = res[i].headers.get('last-modified') || ress.headers.get('last-modified')
+                            p.push(t)
+                        }
+                        resolve(p)
+                    })
+                })
+            })
+
+            const CACHE_COUNT = (() => {
+                let u = 0
+                for (let i in cache_list) {
+                    u += 1
+                }
+                return u
+            })()
+            console.log(CACHE_COUNT)
+            const CACHE_SIZE_COUNT = (() => {
+                t = 0
+                for (let i in cache_list) {
+                    t += cache_list[i].size
+                }
+                return t
+            })()
+            return new Response(dash_main
+                .replace(/<!--CACHE_ACTIVE-->/g, ' active')
+                .replace(/<!--SECTION-->/g, cache_content_html)
+                .replace(/<!--CACHE_COUNT-->/g, CACHE_COUNT)
+                .replace(/<!--CACHE_SIZE_COUNT-->/g, intelligent_size(CACHE_SIZE_COUNT))
+                .replace(/<!--INFO-->/g, (() => {
+                    o = ""
+                    for (var i in cache_list) {
+                        o += cache_section_html
+                            .replace(/<!--URL-->/g, cache_list[i].url)
+                            .replace(/<!--SIZE-->/g, intelligent_size(cache_list[i].size))
+                            .replace(/<!--TIME-->/g, cache_list[i].time)
+                            .replace(/<!--CACHE_ROUND-->/g, (cache_list[i].size / CACHE_SIZE_COUNT * 100).toFixed(2))
+                    }
+                    return o
+                    //cache_section_html
+                })())
+
+                , { status: 200, headers: { 'Content-Type': 'text/html' } })
+            break;
+
+        default:
+            let info = await (await fetch(endpoint + 'part/info.html')).text()
+            let content = await (await fetch(endpoint + 'content/main.html')).text()
+            const HIT_COUNT = Math.round((Object.keys(HIT_ALL).reduce((a, b) => a + HIT_ALL[b], 0)))
+            const HIT_COUNT_SIZE = Math.round((Object.keys(HIT_ALL_SIZE).reduce((a, b) => a + HIT_ALL_SIZE[b], 0)))
+            return new Response(dash_main
+                .replace(/<!--HOME_ACTIVE-->/g, 'active')
+                .replace(/<!--SECTION-->/g, content)
+
+                .replace(/<!--HIT_COUNT-->/g, HIT_COUNT)
+                .replace(/<!--HIT_SIZE_COUNT-->/g, intelligent_size(HIT_COUNT_SIZE))
+                .replace(/<!--INFO-->/g, (() => {
+                    t = ""
+                    for (var i in HIT_ALL) {
+                        t += info
+                            .replace(/<!--DOMAIN-->/g, i)
+                            .replace(/<!--HIT_COUNT-->/g, HIT_ALL[i])
+                            .replace(/<!--HIT_SIZE_COUNT-->/g, intelligent_size(HIT_ALL_SIZE[i]))
+                            .replace(/<!--HIT_ROUND-->/g, (HIT_ALL_SIZE[i] / HIT_COUNT_SIZE * 100).toFixed(2))
+                            .replace(/<!--TYPE-->/g, (() => {
+                                if (blog.plus.indexOf(i) > -1) {
+                                    return "源站"
+                                } else {
+                                    return "静态资源"
+                                }
+                            })())
+                    }
+                    return t
+                })())
+
+
+
+                , { headers: { "content-type": "text/html; charset=utf-8" } })
+    }
 }
-//(async () => { await test_func() })();
-//setInterval(async () => { test_func() }, 10000);*/
