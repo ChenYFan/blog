a25de5376686d240726aa41e5625de794ca7a769
ChenYFan <ChenYFan@users.noreply.github.com>
2022-05-01 01:47:11 +0000

    deploy: 8360ce19b7423cf9af9b2a2fdcbe199a1cc68754

--- sw.js
+++ sw.js
@@ -560,10 +560,13 @@ const handlecgi = async (req) => {
     //console.log(uuid)
     const pathname = urlObj.href.substr(urlObj.origin.length)
     const query = q => urlObj.searchParams.get(q)
-    const endpoint = "https://npm.elemecdn.com/chenyfan-blog-helper-dash@0.0.7/"
-    //const endpoint = "http://127.0.0.1:9393/"
+    //const endpoint = "https://npm.elemecdn.com/chenyfan-blog-helper-dash@0.0.7/"
+    const endpoint = "http://127.0.0.1:45454/"
     let dash_main = await (await fetch(endpoint + 'index.html')).text()
-
+    const priv_config = await db.read('priv_config') ? JSON.parse(await db.read('priv_config')) : {
+        analytics: true,
+        globalcompute: true,
+    }
 
     const HIT_HOT = await (async () => { try { return JSON.parse(await db.read('HIT_HOT')) || {} } catch (e) { return {} } })()
     const HIT_HOT_SIZE = await (async () => { try { return JSON.parse(await db.read('HIT_HOT_SIZE')) || {} } catch (e) { return {} } })()
@@ -606,6 +609,60 @@ const handlecgi = async (req) => {
     })()
     dash_main = dash_main.replace(/<!--MESSAGE-->/g, MSG_HTML)
     switch (query('dash')) {
+        case 'privacy':
+            const privacy_content = [
+                {
+                    "name": "Cookie",
+                    "info": "基础服务",
+                    "star": 5,
+                    "disabled": true,
+                    "svg": ` <path d="M512 128a384 384 0 0 0-384 384 384 384 0 0 0 384 384 384 384 0 0 0 384-384c0-21.333333-1.706667-42.666667-5.546667-64C878.933333 426.666667 853.333333 426.666667 853.333333 426.666667h-85.333333V384c0-42.666667-42.666667-42.666667-42.666667-42.666667h-85.333333V298.666667c0-42.666667-42.666667-42.666667-42.666667-42.666667h-42.666666V170.666667c0-42.666667-42.666667-42.666667-42.666667-42.666667M405.333333 256A64 64 0 0 1 469.333333 320 64 64 0 0 1 405.333333 384 64 64 0 0 1 341.333333 320 64 64 0 0 1 405.333333 256m-128 170.666667A64 64 0 0 1 341.333333 490.666667 64 64 0 0 1 277.333333 554.666667 64 64 0 0 1 213.333333 490.666667 64 64 0 0 1 277.333333 426.666667m213.333334 42.666666a64 64 0 0 1 64 64 64 64 0 0 1-64 64 64 64 0 0 1-64-64 64 64 0 0 1 64-64m213.333333 85.333334a64 64 0 0 1 64 64 64 64 0 0 1-64 64 64 64 0 0 1-64-64 64 64 0 0 1 64-64M469.333333 682.666667a64 64 0 0 1 64 64A64 64 0 0 1 469.333333 810.666667a64 64 0 0 1-64-64A64 64 0 0 1 469.333333 682.666667z" fill="rgb(79 70 229)" p-id="2096"></path>`,
+                    "des": "Cookie是一种技术，它可以帮助我们记住您的信息，以便您能够更好的使用我们的服务。但是在本站,Cookie仅用于基础认证，我们保证不会将其用于其他目的。"
+                },
+                {
+                    //统计
+                    "name": "ChenYFan Analytics",
+                    "id":"analytics",
+                    "info": "统计",
+                    "star": 45,
+                    "disabled": false,
+                    "svg": `<path d="M1019.904 450.56L536.576 557.056l417.792 208.896C999.424 692.224 1024 606.208 1024 512c0-20.48 0-40.96-4.096-61.44z m-12.288-61.44C958.464 184.32 786.432 28.672 573.44 4.096L446.464 512l561.152-122.88zM737.28 970.752c73.728-36.864 139.264-90.112 188.416-159.744L507.904 602.112l229.376 368.64zM512 0C229.376 0 0 229.376 0 512s229.376 512 512 512c61.44 0 118.784-12.288 172.032-28.672L385.024 512 512 0z" p-id="2097" fill="rgb(79 70 229)"></path>`,
+                    "des": "ChenYFan Analytics是由ChenYFan独立开发的一款统计工具，用于记录你在此网站的所有操作。如果禁用此功能，GlobalComputing功能将强制打开"
+                },
+                {
+                    //GlobalComputing
+                    "name": "GlobalComputing",
+                    "id":"globalcompute",
+                    "info": "全球计算",
+                    "star": 20,
+                    "disabled": !priv_config.analytics ,
+                    "svg":`<path d="M919.04 256L528 33.28a32 32 0 0 0-32 0L104.96 256a32 32 0 0 0-16 27.52v451.2a32 32 0 0 0 16 27.52l391.04 225.92a32 32 0 0 0 32 0L919.04 768a32 32 0 0 0 16-27.52v-192a32 32 0 0 0-64 0v153.6l-176-103.04V415.36a16 16 0 0 0-7.68-14.08L528 309.76V106.88l343.04 197.76v85.12a32 32 0 0 0 64 0V286.08a32 32 0 0 0-16-30.08z m-423.04 51.2L344.96 396.8 168.96 295.68l327.04-188.8z m-343.04 13.44l176 104.32v174.08l-176 101.76z m16 405.12L344.96 627.2l151.04 87.04v202.88z m512-101.12l175.36 101.12-328.32 191.36v-202.88z m-16-27.52L512 686.72 360.96 599.04V424.96L512 337.28l151.04 87.04z" fill="rgb(79 70 229)" p-id="3120"></path>`,
+                    "des": "GlobalComputing是一款利用用户闲置计算能力的工具，在用户容忍的范围内提供足够高的算力，用户可以主动关闭。此外，当统计功能被禁用的时候，此功能将强制打开"
+                }
+
+            ]
+            let privacy_html = await (await fetch(endpoint + 'content/privacy.html')).text()
+            let privacy_part = await (await fetch(endpoint + 'part/privacy.html')).text()
+            let privacy_init = ""
+            privacy_content.forEach(async (item) => {
+                privacy_init += privacy_part.replace("{{NAME}}", item.name)
+                    .replace("{{INFO}}", item.info)
+                    .replace("{{STAR}}", item.star)
+                    .replace("{{DES}}", item.des)
+                    .replace("{{SVG}}", item.svg)
+                    .replace("{{DISABLED}}", item.disabled ? "300" : "600")
+                    .replace("{{ABILITY}}", item.disabled ? "不可更改，仅启用基础服务" : (priv_config[item.id] ? "已启用" : "未启用"))
+            })
+            privacy_html = privacy_html.replace(/<!--PRIVACY_CONTENT-->/g, privacy_init)
+            return new Response(
+                dash_main
+                    .replace(/<!--PRIVACY_ACTIVE-->/g, ' active')
+                    .replace(/<!--SECTION-->/g, privacy_html)
+                , {
+                    headers: {
+                        'Content-Type': 'text/html'
+                    }
+                })
         case 'test':
             let test_html = await (await fetch(endpoint + 'content/test.html')).text()
             return new Response(dash_main
